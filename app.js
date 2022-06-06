a6b2ff5c83de0329ff0c8d1cc6c2936eca1c24ab
elboqueronpaco <elboqueronpaco@gmail.com>
2022-06-06 17:26:56 +0200

    creación index.html

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import express from "express"
 import {config} from 'dotenv'
+import { fileURLToPath } from "url"
+import path from 'path'
 config()
 const port = process.env.PORT || 4444
 const app = express()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app
+  //.use(express.static(__dirname + '/public'))
   .get('/', (req, res)=>{
-    res.status(200).json({message: 'Hola a todos'})
+    res.status(200).sendFile(path.join(__dirname,'/public','index.html'))
   })
   .listen(port, () => console.log(`Servidor corriento ${port}`))
